ebf4c954f8a503f4fb7248c97b1802df8bf365d6
许大仙 <1900919313@qq.com>
2025-02-06 21:08:04 +0800

    refactor: 重构项目并添加 HelloWorld 类
    - 移除了 Computer、FlashDrive、Keyboard 和 USB 类
    - 添加了 HelloWorld 类，包含主方法打印"你好，世界！！！"
    - 在 build.gradle 中添加了 gradleApi() 的 compileOnly 依赖
    - 删除了 spring-boot-starter-web 的 implementation 依赖

--- src/main/java/com/github/USB.java
+++
@@ -1,7 +0,0 @@
-package com.github;
-
-// 定义 USB 接口
-public interface USB {
-    void connect();  // 连接 USB 设备
-    void disconnect(); // 断开 USB 设备
-}
---
+++ src/main/java/com/github/day01/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.github.day01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好，世界！！！");
+    }
+}
