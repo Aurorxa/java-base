3f415efca2c08edf2b9861a112e23e9593a6f582
许大仙 <1900919313@qq.com>
2025-02-11 14:55:20 +0800

    feat(day02): 添加整数类型演示代码
    - 新增 IntDemo 类，演示了 byte、short、int 和 long 四种整数类型的定义和使用
    - 代码位于 com.github.day02包下

---
+++ src/main/java/com/github/day02/IntDemo.java
@@ -0,0 +1,29 @@
+package com.github.day02;
+
+/**
+ *
+ *
+ * @author 许大仙
+ * @version 1.0
+ * @since 2025-02-11 14:03
+ */
+public class IntDemo {
+    public static void main(String[] args) {
+
+        // 定义 byte
+        byte b = 100;
+        System.out.println("byte: " + b);
+
+        // 定义 short
+        short s = 1000;
+        System.out.println("short: " + s);
+
+        // 定义 int
+        int i = 10000;
+        System.out.println("int: " + i);
+
+        // 定义 long
+        long l = 10L;
+        System.out.println("long: " + l);
+    }
+}
