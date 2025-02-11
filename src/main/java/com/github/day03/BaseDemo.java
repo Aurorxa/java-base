58808eb0d889e50c8b646bb0c748657e6249bdc2
许大仙 <1900919313@qq.com>
2025-02-11 15:43:45 +0800

    feat(day02): 添加整数类型演示代码
    - 新增 IntDemo 类，演示了 byte、short、int 和 long 四种整数类型的定义和使用
    - 代码位于 com.github.day02包下

---
+++ src/main/java/com/github/day03/BaseDemo.java
@@ -0,0 +1,31 @@
+package com.github.day03;
+
+/**
+ *
+ *
+ * @author 许大仙
+ * @version 1.0
+ * @since 2025-02-11 15:05
+ */
+public class BaseDemo {
+
+    public static void main(String[] args) {
+
+        // 二进制整数
+        int num = 0b1010;
+
+        // 十进制整数（默认）
+        int num2 = 10;
+
+        // 八进制整数
+        int num3 = 012;
+
+        // 十六进制整数
+        int num4 = 0xA;
+
+        System.out.println(num); // 10
+        System.out.println(num2); // 10
+        System.out.println(num3); // 10
+        System.out.println(num4); // 10
+    }
+}
---
+++ src/main/java/com/github/day03/BaseDemo2.java
@@ -0,0 +1,23 @@
+package com.github.day03;
+
+public class BaseDemo2 {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        // 10 对应的二进制是：1010
+        System.out.println(num + " 对应的二进制是：" + Integer.toBinaryString(num));
+        // 10 对应的八进制是：12
+        System.out.println(num + " 对应的八进制是：" + Integer.toOctalString(num));
+        // 10 对应的十六进制是：a
+        System.out.println(num + " 对应的十六进制是：" + Integer.toHexString(num));
+
+        // 10 对应的二进制是： 0b1010
+        System.out.printf(num + " 对应的二进制是： 0b%s\n", Integer.toBinaryString(num));
+        // 10 对应的八进制是： 012
+        System.out.printf(num + " 对应的八进制是： 0%s\n", Integer.toOctalString(num));
+        // 10 对应的十六进制是： 0xa
+        System.out.printf(num + " 对应的十六进制是： 0x%s\n", Integer.toHexString(num));
+    }
+}
