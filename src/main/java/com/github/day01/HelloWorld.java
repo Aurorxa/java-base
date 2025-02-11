02c3567c89dc1457b2f34f72835c8b5236c294c1
许大仙 <1900919313@qq.com>
2025-02-11 10:21:54 +0800

    feat(day02): 添加变量声明和初始化示例
    - 新增 VariableDemo1.java 文件，演示变量声明和赋值
    - 新增 VariableDemo2.java 文件，演示变量初始化
    - 两个示例展示了 int、String 和 double 类型变量的使用
    - 输出变量值，验证变量声明和初始化的正确性

--- src/main/java/com/github/day01/HelloWorld.java
+++ src/main/java/com/github/day01/HelloWorld.java
@@ -1,7 +1,18 @@
 package com.github.day01;
 
+/**
+ * 这是一个入门案例 -- HelloWorld
+ * @author 许大仙
+ * @version 1.0
+ * @since 2025-02-11 08:06
+ */
 public class HelloWorld {
+    /**
+     * 这是一个 main 方法
+     * @param args 命令行参数
+     */
     public static void main(String[] args) {
+        // 这是一个输出语句
         System.out.println("你好，世界！！！");
     }
 }
