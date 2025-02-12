173bbce2e8635fab0f8ef8d17bd87061b1e271e4
许大仙 <1900919313@qq.com>
2025-02-12 14:46:05 +0800

    chore: 更新 .gitignore并添加新示例代码
    - 修改 .gitignore 文件，简化 Intellij IDEA 相关忽略规则- 删除 day02/src/main/java/com/github/ConstantDemo.java 中的多余注释
    - 新增 day03/src/main/java/com/github/BaseDemo3.java 示例代码，演示 post-increment 操作符

--- day02/src/main/java/com/github/ConstantDemo.java
+++ day02/src/main/java/com/github/ConstantDemo.java
@@ -26,6 +26,5 @@ public class ConstantDemo {
         // 浮点类型常量
         System.out.println(3.14F); // 单精度浮点类型常量
         System.out.println(3.14); // 双精度浮点类型常量，可以加d或D，如果不写，默认就是双精度浮点类型常量
-
     }
 }
