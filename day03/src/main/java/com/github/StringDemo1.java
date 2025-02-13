778d8e7be7ff97e05a8595fdc022250e0f9fac4a
许大仙 <1900919313@qq.com>
2025-02-13 09:12:09 +0800

    feat(day03): 添加 StringDemo1 类
    - 新增 StringDemo1 类，演示字符串转换为整数
    - 主要功能：将字符串 "123"转换为整数并输出

---
+++ day03/src/main/java/com/github/StringDemo1.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class StringDemo1 {
+    public static void main(String[] args) {
+
+        String str = "123";
+
+        int num = Integer.parseInt(str);
+
+        System.out.println("num = " + num);
+    }
+}
