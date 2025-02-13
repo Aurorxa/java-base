fb9d1f361a6af585c8ea27ff829c5cda2fbbaa71
许大仙 <1900919313@qq.com>
2025-02-13 08:53:47 +0800

    demo(day03): 添加 ConvertDemo2 类演示浮点数到整数的强制类型转换
    - 新增 ConvertDemo2 类
    - 在 main 方法中演示了从浮点数到整数的强制类型转换
    - 输出结果展示了转换后的整数值

---
+++ day03/src/main/java/com/github/ConvertDemo2.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class ConvertDemo2 {
+
+    public static void main(String[] args) {
+
+        // 强制类型转换，损失精度
+        int i = (int) 3.14;
+        // i = 3
+        System.out.println("i = " + i);
+    }
+}
