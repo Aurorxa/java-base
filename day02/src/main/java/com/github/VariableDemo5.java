579de750912471414555f4671e9081f10754c0cb
许大仙 <1900919313@qq.com>
2025-02-17 22:37:03 +0800

    demo(day02): 添加变量演示代码
    - 新增 VariableDemo3、VariableDemo4 和 VariableDemo5 三个类
    - 演示了变量的声明、赋值和输出
    - 展示了整数变量的简单运算
    - 体现了变量值的可变性

---
+++ day02/src/main/java/com/github/VariableDemo5.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class VariableDemo5 {
+    public static void main(String[] args) {
+
+        int num = 10;
+        System.out.println(num); // 10
+
+        num = 20;
+        System.out.println(num); // 20
+    }
+}
